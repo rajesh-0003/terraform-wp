1040fdf90bdef51e7c17c8966c272db0a013166c
rajesh-0003 <rajesh.p3rkr@gmail.com>
2024-11-06 17:29:43 +0530

    Update igw.tf

--- igw.tf
+++ igw.tf
@@ -1,4 +1,4 @@
 # Creating the Internet Gateway
-resource "aws_internet_gateway" "vinaygateway" {
-  vpc_id = aws_vpc.vinayvpc.id
+resource "aws_internet_gateway" "rajeshgateway" {
+  vpc_id = aws_vpc.rajeshvpc.id
 }
