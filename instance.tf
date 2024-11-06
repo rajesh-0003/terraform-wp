76195a5cf5fae88f32b950d9fdce14eb09f0b533
rajesh-0003 <rajesh.p3rkr@gmail.com>
2024-11-06 17:44:57 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "vinay-1" {
   vpc_security_group_ids = [aws_security_group.rajeshsg.id]
   subnet_id     = aws_subnet.rajsubnet1.id
   associate_public_ip_address = true
-  user_data     = file("py-data.sh")
+  user_data     = file("wp-data.sh")
 
   tags = {
     Name = "tf python"
