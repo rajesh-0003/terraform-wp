69a6c2b132beb595faaaae51d3def3c451533d77
rajesh-0003 <rajesh.p3rkr@gmail.com>
2024-11-06 17:46:22 +0530

    Update wp-data.sh

--- wp-data.sh
+++ wp-data.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-sudo yum install git -y
-sudo git clone https://github.com/vinay18-2/wordpress-1.git
-sudo yum install docker -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
-sudo chmod 666 /var/run/docker.sock
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/vinay18-2/wordpress-1.git
-sudo mv wordpress-1/* /home/ec2-user/
-cd /home/ec2-user/
-docker-compose up -d
+sudo yum -y update
+sudo yum -y install git
+sudo yum -y install python3-pip
+git clone https://github.com/rajesh-0003/MyFuel.git
+sudo mv MyFuel/ /home/ec2-user/
+cd /home/ec2-user/MyFuel
+pip3 install -r requirements.txt
+python3 app.py
