d188ae23285cf796cca249c67ece22c68160ec80
rajesh-0003 <rajesh.p3rkr@gmail.com>
2024-11-06 17:43:41 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,34 +1,34 @@
 # Defining CIDR block for the VPC
-variable "vinayvpc_cidr" {
+variable "rajeshvpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
 # Defining CIDR block for subnet 1
-variable "vinaypsubnet1_cidr" {
+variable "rajsubnet1_cidr" {
   default     = "10.0.1.0/24"
 }
 
 # Defining CIDR block for subnet 2
-variable "vinaypsubnet2_cidr" {
+variable "rajsubnet2_cidr" {
   default     = "10.0.2.0/24"
 }
 
 # Defining CIDR block for subnet 3
-variable "vinaypsubnet3_cidr" {
+variable "rajsubnet3_cidr" {
   default     = "10.0.3.0/24"
 }
 
 # Defining CIDR block for subnet 4
-variable "vinaypsubnet4_cidr" {
+variable "rajsubnet4_cidr" {
   default     = "10.0.4.0/24"
 }
 
 # Defining CIDR block for subnet 5
-variable "vinayprsubnet5_cidr" {
+variable "rajpsubnet5_cidr" {
   default     = "10.0.5.0/24"
 }
 
 # Defining CIDR block for subnet 6
-variable "vinayprsubnet6_cidr" {
+variable "rajpsubnet6_cidr" {
   default     = "10.0.6.0/24"
 }
