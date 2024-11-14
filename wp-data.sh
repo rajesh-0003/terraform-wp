fb69551c0798f78f4cf150b59f4d11fc906edc1d
rajesh-0003 <rajesh.p3rkr@gmail.com>
2024-11-14 16:50:06 +0530

    Update wp-data.sh

--- wp-data.sh
+++ wp-data.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo yum -y update
 sudo yum -y install git
 sudo yum -y install python3-pip
 git clone https://github.com/rajesh-0003/MyFuel.git
 sudo mv MyFuel/ /home/ec2-user/
-cd /home/ec2-user/MyFuel
+cd /home/ec2-user/MyFuel/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
